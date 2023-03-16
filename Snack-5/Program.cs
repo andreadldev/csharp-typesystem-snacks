83c27f20243c2b39a95d0b39b634f0844ecc4b54
andreadldev <andrea.delisi2@gmail.com>
2023-03-16 15:32:50 +0100

    Snack 5

---
+++ Snack-5/Program.cs
@@ -0,0 +1,12 @@
+﻿//Snack 5
+Console.WriteLine("Inserisci un numero");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"{num} è pari");
+}
+else
+{
+    Console.WriteLine($"{num} è dispari. Il numero successivo è {num + 1}");
+}
