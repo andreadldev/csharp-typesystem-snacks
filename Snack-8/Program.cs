d78140392c3a78bb3b7e1aa16b5d4bc893e5fbcd
andreadldev <andrea.delisi2@gmail.com>
2023-03-16 17:13:13 +0100

    Snack 8

---
+++ Snack-8/Program.cs
@@ -0,0 +1,21 @@
+﻿//Snack 8
+int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int sum = 0;
+
+Console.Write("Lista numeri:");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i]+" ");
+}
+
+string numbersToString = String.Join("", Array.ConvertAll(numbers, x => x.ToString()));
+Console.Write("\nNumeri in posizione dispari: ");
+for (int i = 0; i < numbersToString.Length; i++)
+{
+    if (numbers[i] % 2 != 0)
+    {
+        Console.Write(numbers[i] + " ");
+        sum += numbers[i];
+    }
+}
+Console.WriteLine($"\nSomma dei numeri in posizione dispari: {sum}");
