1c572a55e81fb475d9dc76590bdccd3fa40b2d96
andreadldev <andrea.delisi2@gmail.com>
2023-03-16 14:30:13 +0100

    Aggiungere i file di progetto.

---
+++ Snack-1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Snack 1
+Console.WriteLine("Inserisci un numero");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci un secondo numero");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"Il numero più grande è {num1}");
+}
+else
+{
+    Console.WriteLine($"Il numero più grande è {num2}");
+}
