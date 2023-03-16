5eaadf0142ec3cec1a0470323704916b2bb52ec7
andreadldev <andrea.delisi2@gmail.com>
2023-03-16 16:25:52 +0100

    Snack 7

---
+++ Snack-7/Program.cs
@@ -0,0 +1,14 @@
+﻿//Snack 7
+int[] numbers = new int[6];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Inserisci un numero (ciclo {i + 1})");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    if (numbers[i] % 2 != 0)
+    {
+        sum += numbers[i];
+    }
+}
+Console.WriteLine($"La somma dei numeri dispari inseriti è {sum}");
