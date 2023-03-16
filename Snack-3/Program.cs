e6705d3cabf64a058be0fd52e6d147d0f724b65f
andreadldev <andrea.delisi2@gmail.com>
2023-03-16 14:59:29 +0100

    Snack 3

---
+++ Snack-3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Snack 3
+int[] numbers = new int[10];
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"Inserisci un numero (ciclo {i})");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+    sum += numbers[i];
+}
+Console.WriteLine($"La somma dei numeri inseriti è {sum}");
