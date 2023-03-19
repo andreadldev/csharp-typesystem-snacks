da0f5812d5d730c9e04b15dcafb98398c7575ebc
andreadldev <andrea.delisi2@gmail.com>
2023-03-19 15:47:40 +0100

    Snack 12

---
+++ Snack-12/Program.cs
@@ -0,0 +1,16 @@
+﻿//Snack 12
+Console.WriteLine("Inserisci un numero");
+int input = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Il numero inserito è {checkEvenOdd(input)}");
+string checkEvenOdd(int num)
+{
+    if (num % 2 == 0)
+    {
+        return "pari";
+    }
+    else
+    {
+        return "dispari";
+    }
+}
